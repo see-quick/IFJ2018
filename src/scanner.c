21e8b8ef61b0ad207bb42857062c65ed7f633dcd
Marek Rohel <rohyqq@gmail.com>
2018-10-04 18:13:41 +0200

    Scanner number fix

--- src/scanner.c
+++ src/scanner.c
@@ -343,7 +343,7 @@ int getToken(){
                         if (flag && zero_cnt > 1){
                             return ERROR_LEX;
                         }
-                        else if(isspace(c) || c == ',' || c == ')'){     // is delimiter     && digit_lock == false
+                        else if(isspace(c) || c == ',' || c == ')' || c == '+' || c == '-' || c == '*' || c == '/'){     // is delimiter     && digit_lock == false
                             expr = true;
                             return LEX_NUMBER;
                         }
